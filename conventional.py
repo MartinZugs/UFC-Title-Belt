a574baaefd548acce70553b2c0bbaaf95f958b1a
Jay Kinzie <jaykinzie@gmail.com>
2019-11-20 01:19:46 -0600

    did work sklearn work in the conventional solution

--- conventional.py
+++ conventional.py
@@ -1,43 +1,20 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
-
-import sys
-
-class Result:
-    def __init__(self):
-        pass
-
-
-def run_k_fold_batch(in_data_x, in_data_y, in_k_fold, in_random_state):
-
-    # k fold definition
-    kf = KFold(n_splits=K_FOLDS,shuffle=True, random_state=RANDOM_STATE)
-    kf.get_n_splits()
-
-    for train_index, test_index in kf.split(pd_data_x):
-
-        print()
-        # print("TRAIN:", train_index, "TEST:", test_index)
-
-        x_train, x_test = pd_data_x.iloc[train_index], pd_data_x.iloc[test_index]
-        y_train, y_test = pd_data_y.iloc[train_index], pd_data_y.iloc[test_index]
-
-        logreg = LogisticRegression(solver='lbfgs')
-        logreg.fit(x_train, y_train)
-        score = logreg.score(x_test, y_test)
-        print( score )
-
+from sklearn.linear_model import Perceptron
 
 if __name__ == '__main__':
-
     # path to the csv file
     DATA_PATH = "manually-preprocessed_data-full-headers.csv"
     # number of folds
     K_FOLDS = 10
+    # maximum iterations
+    MAX_ITER = 20000
     # random state to use for shuffling
     RANDOM_STATE = 987654321
+    # multi-core threads
+    MULTI_CORE = -1
 
     # import data
     pd_data = pd.read_csv(DATA_PATH)
@@ -46,6 +23,40 @@ if __name__ == '__main__':
     pd_data_y = pd_data["Winner"]
     pd_data_x = pd_data.loc[:, pd_data.columns != "Winner"]
 
-    run_k_fold_batch(in_data_x=pd_data_x, in_data_y=pd_data_y, in_k_fold=K_FOLDS, in_random_state=RANDOM_STATE)
+    ####################################################################################################################
+    print()
+    print("RUNNING PERCEPTRON")
+    # parameters to search over
+    per_parameters = {'penalty': ('None', 'l1', 'l2', 'elasticnet'),
+                      'warm_start': ('True', 'False'),
+                      'class_weight': (None, 'balanced')
+                      } #   'eta0': ('1', '.1', '.01'), 'fit_intercept': ('True', 'False')
 
+    # logistic regression definition
+    per = Perceptron(shuffle=True, random_state=RANDOM_STATE, max_iter=MAX_ITER, verbose=10) # n_jobs=MULTI_CORE
+    # gridsearch over the parameters
+    clf = GridSearchCV(per, per_parameters, cv=K_FOLDS, n_jobs=MULTI_CORE, verbose=10)
+    # fit the date
+    clf.fit(pd_data_x, pd_data_y)
 
+    # result = sorted(clf.cv_results_.keys())
+    print()
+    print("PERCEPTRON RESULTS:")
+    print(clf.cv_results_["mean_test_score"])
+
+    ####################################################################################################################
+    print()
+    print("RUNNING LOGISTIC REGRESSION")
+    # parameters to search over
+    log_parameters = {'solver': ('lbfgs', 'liblinear'), 'C': [1, 10]}
+    # logistic regression definition
+    log_reg = LogisticRegression(random_state=RANDOM_STATE, max_iter=MAX_ITER)
+    # gridsearch over the parameters
+    clf = GridSearchCV(log_reg, log_parameters, cv=K_FOLDS, n_jobs=MULTI_CORE, verbose=10)
+    # fit the date
+    clf.fit(pd_data_x, pd_data_y)
+
+    # result = sorted(clf.cv_results_.keys())
+    print()
+    print("LOGISTIC REGRESSION RESULTS:")
+    print(clf.cv_results_["mean_test_score"])
