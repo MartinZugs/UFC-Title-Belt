1a6215af0e384a950c57914cc0d8d49b67fb6453
Martin Zugschwert <martin.e.zugschwert@gmail.com>
2019-12-03 17:16:46 -0600

    Adding grid search to the keras

--- keras_code.py
+++ keras_code.py
@@ -7,26 +7,44 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
 # path to the csv file
 DATA_PATH = "manually-preprocessed_data-full-headers.csv" #'preprocessed_data_condensed.csv' # "manually-preprocessed_data-full-headers.csv"
+
 # import data
 pd_data = pd.read_csv(DATA_PATH)
+
 # x and y data
 pd_data_y = pd_data["Winner"]
 pd_data_x = pd_data.loc[:, pd_data.columns != "Winner"]
 
-# baseline model
-def create_baseline():
+# Function to create model, required for KerasClassifier
+def create_model(optimizer='rmsprop', init='glorot_uniform'):
 	# create model
 	model = Sequential()
-	model.add(Dense(2000, input_dim=159, activation='relu'))
-	model.add(Dense(1, activation='sigmoid'))
+	model.add(Dense(12, input_dim=159, kernel_initializer=init, activation='relu'))
+	model.add(Dense(8, kernel_initializer=init, activation='relu'))
+	model.add(Dense(1, kernel_initializer=init, activation='sigmoid'))
 	# Compile model
-	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 	return model
 
 # evaluate model with standardized dataset
-estimator = KerasClassifier(build_fn=create_baseline, epochs=100, verbose=1)
-kfold = StratifiedKFold(n_splits=10, shuffle=True)
-results = cross_val_score(estimator, pd_data_x, pd_data_y, cv=kfold)
-print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+model = KerasClassifier(build_fn=create_model, epochs=100, verbose=0) #verbose = 0 for nothing to show
+# grid search epochs, batch size and optimizer
+optimizers = ['rmsprop', 'adam']
+init = ['glorot_uniform', 'normal', 'uniform']
+epochs = [50, 100, 150]
+batches = [5, 10, 20]
+param_grid = dict(optimizer=optimizers, epochs=epochs, batch_size=batches, init=init)
+grid = GridSearchCV(estimator=model, param_grid=param_grid)
+grid_result = grid.fit(pd_data_x, pd_data_y)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+	print("%f (%f) with: %r" % (mean, stdev, param))
