b585aa97f33f7e8fa6fe84ec76aabeb3da2ef457
Jonathan <warkentin.jon@gmail.com>
2019-12-06 11:04:40 -0600

    add pickled classifiers

--- conventional.py
+++ conventional.py
@@ -32,84 +32,84 @@ if __name__ == '__main__':
 
 
 
-    ####################################################################################################################
-    print()
-    print("RUNNING SVM")
-    # parameters to search over
-    svm_parameters = {'C': [.000001,.00001,.0001, .001,.01, .1],  # np.arange(.01, 10, .1)
-                      'gamma': [.000001,.00001,.0001, .001,.01, .1]} # np.arange(.01, 10, .01)
-                      #'gamma':('auto', 'scale')} # 'kernel':'rbf', #, 'linear', 'sigmoid'
-
-    # logistic regression definition
-    svm = SVC(random_state=RANDOM_STATE, verbose=VERBOSE)  # n_jobs=MULTI_CORE ,max_iter=MAX_ITER
-    # gridsearch over the parameters
-    clf = GridSearchCV(svm, svm_parameters, cv=K_FOLDS, n_jobs=MULTI_CORE, verbose=VERBOSE)
-    # fit the date
-    clf.fit(pd_data_x, pd_data_y)
-
-    # save trained SVM classifier to file
-    svm_outfile = open("svm_clf.pickle", "w+b")
-    pickle.dump(clf, svm_outfile)
-    svm_outfile.close()
-
-    best_score_svm = clf.best_score_
-    best_params_svm = clf.best_params_
-
-    print("")
-    print("")
-    print("SVM RESULTS:")
-    print("BEST SCORE: ", best_score_svm)
-    print("BEST PARAMS: ", best_params_svm)
-
-    # wait for 10 seconds so the user can see the score!
-    time.sleep(10)
-
-    ####################################################################################################################
-    print()
-    print("RUNNING PERCEPTRON")
-    # parameters to search over
-    per_parameters = {'penalty': ('None', 'l1', 'l2', 'elasticnet'),
-                      'warm_start': ('True', 'False'),
-                      'class_weight': (None, 'balanced')
-                      }  # 'eta0': ('1', '.1', '.01'), 'fit_intercept': ('True', 'False')
-
-    # logistic regression definition
-    per = Perceptron(shuffle=True, random_state=RANDOM_STATE, max_iter=MAX_ITER, verbose=VERBOSE)  # n_jobs=MULTI_CORE
-    # gridsearch over the parameters
-    clf = GridSearchCV(per, per_parameters, cv=K_FOLDS, n_jobs=MULTI_CORE, verbose=VERBOSE)
-    # fit the date
-    clf.fit(pd_data_x, pd_data_y)
-
-    # save trained perceptron classifier to file
-    per_outfile = open("per_clf.pickle", "w+b")
-    pickle.dump(clf, per_outfile)
-    per_outfile.close()
-
-    best_score_perceptron = clf.best_score_
-    best_params_perceptron = clf.best_params_
-
-    print("")
-    print("")
-    print("PERCEPTRON RESULTS:")
-    print("BEST SCORE: ", best_score_perceptron)
-    print("BEST PARAMS: ", best_params_perceptron)
-
-    # wait for 10 seconds so the user can see the score!
-    time.sleep(10)
+    # ####################################################################################################################
+    # print()
+    # print("RUNNING SVM")
+    # # parameters to search over
+    # svm_parameters = {'C': [.000001,.00001,.0001, .001,.01, .1],  # np.arange(.01, 10, .1)
+    #                   'gamma': [.000001,.00001,.0001, .001,.01, .1]} # np.arange(.01, 10, .01)
+    #                   #'gamma':('auto', 'scale')} # 'kernel':'rbf', #, 'linear', 'sigmoid'
+    #
+    # # logistic regression definition
+    # svm = SVC(random_state=RANDOM_STATE, verbose=VERBOSE)  # n_jobs=MULTI_CORE ,max_iter=MAX_ITER
+    # # gridsearch over the parameters
+    # clf = GridSearchCV(svm, svm_parameters, cv=K_FOLDS, n_jobs=MULTI_CORE, verbose=VERBOSE)
+    # # fit the date
+    # clf.fit(pd_data_x, pd_data_y)
+    #
+    # # save trained SVM classifier to file
+    # svm_outfile = open("svm_clf.pickle", "w+b")
+    # pickle.dump(clf, svm_outfile)
+    # svm_outfile.close()
+    #
+    # best_score_svm = clf.best_score_
+    # best_params_svm = clf.best_params_
+    #
+    # print("")
+    # print("")
+    # print("SVM RESULTS:")
+    # print("BEST SCORE: ", best_score_svm)
+    # print("BEST PARAMS: ", best_params_svm)
+    #
+    # # wait for 10 seconds so the user can see the score!
+    # time.sleep(10)
+    #
+    # ####################################################################################################################
+    # print()
+    # print("RUNNING PERCEPTRON")
+    # # parameters to search over
+    # per_parameters = {'penalty': ('None', 'l1', 'l2', 'elasticnet'),
+    #                   'warm_start': ('True', 'False'),
+    #                   'class_weight': (None, 'balanced')
+    #                   }  # 'eta0': ('1', '.1', '.01'), 'fit_intercept': ('True', 'False')
+    #
+    # # logistic regression definition
+    # per = Perceptron(shuffle=True, random_state=RANDOM_STATE, max_iter=MAX_ITER, verbose=VERBOSE)  # n_jobs=MULTI_CORE
+    # # gridsearch over the parameters
+    # clf = GridSearchCV(per, per_parameters, cv=K_FOLDS, n_jobs=MULTI_CORE, verbose=VERBOSE)
+    # # fit the date
+    # clf.fit(pd_data_x, pd_data_y)
+    #
+    # # save trained perceptron classifier to file
+    # per_outfile = open("per_clf.pickle", "w+b")
+    # pickle.dump(clf, per_outfile)
+    # per_outfile.close()
+    #
+    # best_score_perceptron = clf.best_score_
+    # best_params_perceptron = clf.best_params_
+    #
+    # print("")
+    # print("")
+    # print("PERCEPTRON RESULTS:")
+    # print("BEST SCORE: ", best_score_perceptron)
+    # print("BEST PARAMS: ", best_params_perceptron)
+    #
+    # # wait for 10 seconds so the user can see the score!
+    # time.sleep(10)
 
     ####################################################################################################################
     print()
     print("RUNNING LOGISTIC REGRESSION")
     # parameters to search over
-    log_parameters = {'penalty': ('l1', 'l2'),
+    log_parameters = {'penalty': ['l2'],
                       'warm_start': ('True', 'False'),
                       'class_weight': (None, 'balanced'),
-                      'C': np.arange(4, 5, .01)} # 'C': [8,9,9.5,10,10.5,11,12]
+                      'C': [.00001, .0001, .001, .01, .1, 1, 10, 100, 1000, 10000, 100000]}#np.arange(4, 5, .01)} # 'C': [8,9,9.5,10,10.5,11,12]
 
     # logistic regression definition
     log_reg = LogisticRegression(solver='lbfgs', random_state=RANDOM_STATE, n_jobs=MULTI_CORE, max_iter=MAX_ITER, verbose=VERBOSE)
     # gridsearch over the parameters
-    clf = GridSearchCV(log_reg, log_parameters, cv=K_FOLDS, n_jobs=MULTI_CORE, verbose=VERBOSE)
+    clf = GridSearchCV(log_reg, log_parameters, cv=K_FOLDS, n_jobs=MULTI_CORE, verbose=10)
     # fit the date
     clf.fit(pd_data_x, pd_data_y)
 
@@ -125,17 +125,17 @@ if __name__ == '__main__':
     time.sleep(10)
 
 
-    print("")
-    print("")
-    print("SVM RESULTS:")
-    print("BEST SCORE: ", best_score_svm)
-    print("BEST PARAMS: ", best_params_svm)
-
-    print("")
-    print("")
-    print("PERCEPTRON RESULTS:")
-    print("BEST SCORE: ", best_score_perceptron)
-    print("BEST PARAMS: ", best_params_perceptron)
+    # print("")
+    # print("")
+    # print("SVM RESULTS:")
+    # print("BEST SCORE: ", best_score_svm)
+    # print("BEST PARAMS: ", best_params_svm)
+    #
+    # print("")
+    # print("")
+    # print("PERCEPTRON RESULTS:")
+    # print("BEST SCORE: ", best_score_perceptron)
+    # print("BEST PARAMS: ", best_params_perceptron)
 
     print("")
     print("")
