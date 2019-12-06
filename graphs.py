079cdc68659bd411e6470ec785e5a174b146f277
Jonathan <warkentin.jon@gmail.com>
2019-12-06 13:17:40 -0600

    in graphs.py read in pickled classifiers and print info on best estimator from grid search

--- graphs.py
+++ graphs.py
@@ -1,13 +1,60 @@
 import pickle
+from sklearn.linear_model import LogisticRegression
 
-example_dict = {1: "6", 2: "2", 3: "f"}
+# load pickled classifiers
+lr_pickle = open("lr_clf.pickle", "rb")
+lr_clf = pickle.load(lr_pickle)
+lr_pickle.close()
 
-pickle_out = open("example.pickle", "w+b")
-pickle.dump(example_dict, pickle_out)
-pickle_out.close()
+svm_pickle = open("svm_clf.pickle", "rb")
+svm_clf = pickle.load(svm_pickle)
+svm_pickle.close()
 
-pickle_in = open("example.pickle", "rb")
-example_dict2 = pickle.load(pickle_in)
-print(example_dict2)
-print(example_dict2[2])
-pickle_in.close()
+per_pickle = open("per_clf.pickle", "rb")
+per_clf = pickle.load(per_pickle)
+per_pickle.close()
+
+# print best models and scores for each classifier
+print("____________________________________________________________________")
+print("LR GRID SEARCH:")
+print("____________________________________________________________________")
+print("Best Estimator:")
+print(lr_clf.best_estimator_)
+print("Best Score:")
+print(lr_clf.best_score_)
+print("Best Params:")
+print(lr_clf.best_params_)
+print("Best Index:")
+print(lr_clf.best_index_)
+print()
+
+print("____________________________________________________________________")
+print("SVM GRID SEARCH:")
+print("____________________________________________________________________")
+print("Best Estimator:")
+print(svm_clf.best_estimator_)
+print("Best Score:")
+print(svm_clf.best_score_)
+print("Best Params:")
+print(svm_clf.best_params_)
+print("Best Index:")
+print(svm_clf.best_index_)
+print()
+
+print("____________________________________________________________________")
+print("PERCEPTRON GRID SEARCH:")
+print("____________________________________________________________________")
+print("Best Estimator:")
+print(per_clf.best_estimator_)
+print("Best Score:")
+print(per_clf.best_score_)
+print("Best Params:")
+print(per_clf.best_params_)
+print("Best Index:")
+print(per_clf.best_index_)
+print()
+
+# train logistic regression with best classifier
+
+# lr = LogisticRegression(solver='lbfgs', random_state=, n_jobs=, max_iter=, verbose=)
+
