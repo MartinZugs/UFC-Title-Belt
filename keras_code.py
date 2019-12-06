417a7490054764f5d759a343b4edbbedd7e738d4
Jay Kinzie <jaykinzie@gmail.com>
2019-12-06 15:37:04 -0600

    Got variable numbers of layers to add as parameters

--- keras_code.py
+++ keras_code.py
@@ -1,23 +1,29 @@
 # Binary Classification with Sonar Dataset: Baseline
+import sys
 import pandas as pd
 from pandas import read_csv
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
+# from pactools.grid_search import GridSearchCVProgressBar
 from sklearn.metrics import accuracy_score
 from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
 
 # path to the csv file
-DATA_PATH = "manually-preprocessed_data-full-headers.csv"  # 'preprocessed_data_condensed.csv' # "manually-preprocessed_data-full-headers.csv"
+DATA_PATH = "manually-preprocessed_data-full-headers.csv"
+# processors
 PROCESSORS = 3
+# K folds
 KFOLD = 10
-VERBOSE = 1
+# verbosity
+VERBOSE = 2
+# random sttate
 RANDOM_STATE = 314159
 
 # import data
@@ -27,39 +33,29 @@ pd_data = shuffle(pd_data, random_state=RANDOM_STATE)
 # x and y data
 pd_data_y = pd_data["Winner"]
 pd_data_x = pd_data.loc[:, pd_data.columns != "Winner"]
-
+# separate training and test data
 pd_data_x_train, pd_data_x_test, pd_data_y_train, pd_data_y_test = train_test_split(pd_data_x, pd_data_y, test_size=.2,
                                                                                     random_state=RANDOM_STATE)
 
-def create_model_gridsearch(optimizer='rmsprop', init='glorot_uniform',_depth=2, _node_start=128, _node_iterate=16):
-    # create model
+
+# Create the KerasClassifier
+def create_model(optimizer='rmsprop', kernel_init='glorot_uniform', activation='sigmoid', layers=1):
+    # create squential model
     model = Sequential()
-    model.add(Dense(128, input_dim=159, kernel_initializer=init, activation='sigmoid'))
+    # add initial layer
+    model.add(Dense(32, input_dim=DATA_SHAPE, kernel_initializer=kernel_init, activation=activation))
+    model.add(Dropout(0.5))
 
-    # add model loop here
+    # add numbers of intermediate layers
+    i = 1
+    for layer in range(layers):
+        print("ADDING LAYER : ", i)
+        model.add(Dense(32, kernel_initializer=kernel_init, activation=activation))
+        model.add(Dropout(0.5))
+        i = i + 1
 
-
-    model.add(Dense(1, kernel_initializer=init, activation='sigmoid'))
-    # Compile model
-    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    return model
-
-
-# Function to create model, required for KerasClassifier
-def create_model(optimizer='rmsprop', init='glorot_uniform'):
-    # create model
-    model = Sequential()
-    model.add(Dense(128, input_dim=159, kernel_initializer=init, activation='sigmoid'))
-    # model.add(Dense(128, kernel_initializer=init, activation='relu'))
-    model.add(Dense(64, kernel_initializer=init, activation='sigmoid'))
-    model.add(Dense(32, kernel_initializer=init, activation='sigmoid'))
-    # model.add(Dense(12, input_dim=159, kernel_initializer=init, activation='relu'))
-    model.add(Dense(16, kernel_initializer=init, activation='sigmoid'))
-    # model.add(Dense(12, kernel_initializer=init, activation='relu'))
-    model.add(Dense(8, kernel_initializer=init, activation='sigmoid'))
-    model.add(Dense(4, kernel_initializer=init, activation='sigmoid'))
-    model.add(Dense(2, kernel_initializer=init, activation='sigmoid'))
-    model.add(Dense(1, kernel_initializer=init, activation='sigmoid'))
+    # add the final layer
+    model.add(Dense(1, kernel_initializer=kernel_init, activation='sigmoid'))
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
     return model
@@ -67,13 +63,24 @@ def create_model(optimizer='rmsprop', init='glorot_uniform'):
 
 # evaluate model with standardized dataset
 model = KerasClassifier(build_fn=create_model, epochs=100, verbose=VERBOSE)  # verbose = 0 for nothing to show
-# grid search epochs, batch size and optimizer
-optimizers = ['rmsprop', 'adam']
-init = ['glorot_uniform', 'normal', 'uniform']
-epochs = [300]
+# grid search parameters
+optimizers = ['rmsprop', 'adam', 'sgd']
+kernel_inits = ['glorot_uniform', 'normal', 'uniform']
+activations = ['sigmoid', 'relu']
+layers = [1, 10]
+epochs = [1000]
 batches = [20]
-param_grid = dict(optimizer=optimizers, epochs=epochs, batch_size=batches, init=init)
+
+# shape of the data. This defines all of the inputs
+DATA_SHAPE = pd_data_x_train.shape[1]
+DATA_LENGTH = pd_data_x_train.shape[0]
+
+# define parameter grid
+param_grid = dict(optimizer=optimizers, epochs=epochs, batch_size=batches, kernel_init=kernel_inits,
+                  activation=activations, layers=layers)
+# define gridsearch
 grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=PROCESSORS, cv=KFOLD, verbose=VERBOSE)
+# fit
 grid_result = grid.fit(pd_data_x_train, pd_data_y_train)
 
 # summarize results
